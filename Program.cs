01ad135a11e5fd3861ca9d6ad0e1c4d31cba9fbf
deepwaterooo <heyan.huang2020@gmail.com>
2023-01-04 22:42:20 -0800

    LOVE MY DEAR COUSIN. web socket protocols. LOVE MY DEAR COUSIN - MUST MARRY HIM AS SOON AS POSSIBLE

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Reflection;
+
 namespace DW.Server.App {
     class Program {
 
 // 选用一种查找比较快速的数据结构:字典 服务器就必须要有个[数据结构]用来存放(维护管理)所有连接着它的客户端
 // 可是问题是：这不是多纯种不案例的字典吗？他又没有用多纯种案例类型的ConcurrentDictionary之类的？也没有上锁？        
-        static Dictionary<Socket, ClientState> clients = new Dictionary<Socket, ClientState>();
+        public static Dictionary<Socket, ClientState> clients = new Dictionary<Socket, ClientState>();
 
 // 用Select来改写服务器端.         
         // checkRead列表
@@ -33,6 +35,7 @@ namespace DW.Server.App {
                 Console.WriteLine("接收客户端连接错误: " + e.Message);
             }
         }
+        
 // 要完善BeginReceive的回调函数了.当服务器确认和客户端建立起连接之后,开始回调这个方法
         public static void ReceiveCallback(IAsyncResult ar)　{
             try　{
@@ -58,14 +61,7 @@ namespace DW.Server.App {
             }
         }
         //public Socket EndAccept (IAsyncResult asyncResult) { }
-// 服务器向所有客户端广播消息：不完整的代码 片段
-// 这个功能: 客户端 还没实现        
-        public void broadcastMsgs(byte[] sendBytes) { // 遍历服务器所维护的所有连接着的客户端,拿到客户端信道索引,异步发消息
-            foreach (ClientState  item in clients.Values) {
-// 这里，它前面说，好像是先发到操作系统底层的缓冲区，然后我们程序员就不用管了 ？ 操作系统会帮我们完成管道发送 ？                
-                item.socket.Send(sendBytes); // 这里应该也是异步的? 还是说对于这个客户端的信道来说,是对于客户端来说的同步收消息呢(对于服务器端来说是异步)?
-            }
-        }
+
         public static void ReadListenfd(Socket listenfd) {
             Console.WriteLine("Accpet");
             Socket clientfd = listenfd.Accept();
@@ -73,6 +69,8 @@ namespace DW.Server.App {
             state.socket = clientfd ;
             clients.Add(clientfd, state);
         }
+
+// 处理每个在线客户端的消息        
         public static bool ReadClientfd(Socket clientfd) {
             ClientState state = clients[clientfd];
             // 接收字节
@@ -81,6 +79,11 @@ namespace DW.Server.App {
                 count = clientfd.Receive(state.readBuff);
             }
             catch (SocketException e) {
+                // 下线时调用反射
+                MethodInfo mei = typeof(MyServer.EventHandler).GetMethod("OnDisConnect");
+                object[] ob = { state };
+                mei.Invoke(null, ob);
+                
                 clientfd.Close();
                 clients.Remove(clientfd);
                 Console.WriteLine("异常报告: "+ e.Message);
@@ -88,23 +91,45 @@ namespace DW.Server.App {
             }
             // 如果客户端强行下线
             if (count <= 0) {
+                // 下线时调用反射
+                MethodInfo mei = typeof(MyServer.EventHandler).GetMethod("OnDisConnect");
+                object[] ob = { state };
+                mei.Invoke(null, ob);
+
                 clientfd.Close();
                 clients.Remove(clientfd);
                 Console.WriteLine("客户端　socket 已关闭");
                 return false;
             }
-            // 数据分发
+            // 数据处理: 数据的解析
             string recvStr = Encoding.Default.GetString(state.readBuff, 0, count);
-            Console.WriteLine("Receive :　"+　recvStr );
-            // 客户端的IP地址
-            string sendStr = clientfd.RemoteEndPoint.ToString() + ":" + recvStr;
-            byte[] sendBytes = Encoding.Default.GetBytes(sendStr);
-            foreach (ClientState item in clients.Values ) {　// 广播
-                item.socket.Send(sendBytes);
-            }
+            Console.WriteLine("Receive :　"+　recvStr ); // 比如客户端来的消息是:Enter|127.0.0.1:12315
+            string[] split = recvStr.Split('|');
+            string msgName = split[0];
+            string msgArgs = split[1]; // bug here TODO
+            string funName = "Msg" + msgName;
+            MethodInfo mi = typeof(MsgHandler).GetMethod(funName);
+            object[] o = { state, msgArgs };
+            mi.Invoke(null, o);
+// 将客户端的发送代码注释掉了 // <<<<<<<<<<<<<<<<<<<<           
+//             // 需要发送给客户端的
+//             // string sendStr = clientfd.RemoteEndPoint.ToString() + ":" + recvStr;
+//             // byte[] sendBytes = Encoding.Default.GetBytes(sendStr);
+//             string sendStr = recvStr;
+//             byte[] sendBytes = Encoding.Default.GetBytes(sendStr);
+//             foreach (ClientState item in clients.Values ) {　// 广播: 遍历服务器所维护的所有连接着的客户端,拿到客户端信道索引,异步发消息
+// // 这里，它前面说，好像是先发到操作系统底层的缓冲区，然后我们程序员就不用管了 ？ 操作系统会帮我们完成管道发送 ？
+//                     item.socket.Send(sendBytes); // 这里应该也是异步的? 还是说对于这个客户端的信道来说,是对于客户端来说的同步收消息呢(对于服务器端来说是异步)?
+//             }
             return true;
         }
-        
+// 我们将客户端的发送代码注释掉了,所以我们也要单独写一个方法,来广播消息: // <<<<<<<<<<<<<<<<<<<< 
+// 发送
+        public static void Send(ClientState cs, string sendStr) {
+            byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
+            cs.socket.Send(sendBytes);
+        }
+
         static void Main(string [] args) {
             Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
